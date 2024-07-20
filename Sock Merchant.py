d5f59114db1549b9fff6c0e9f83651cbb19728ca
Athes <168402508+Atheswar@users.noreply.github.com>
2024-07-20 19:05:55 +0530

    Create Sock Merchant.py

---
+++ Sock Merchant.py
@@ -0,0 +1,8 @@
+def sock_merchant(n, ar):
+    from collections import Counter
+    sock_counts = Counter(ar)
+    return sum(count // 2 for count in sock_counts.values())
+
+if __name__ == "__main__":
+    ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+    print(sock_merchant(len(ar), ar))
